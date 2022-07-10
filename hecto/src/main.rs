38dc0b4477c4c6d0078a3b480342f271fd4956e2
takashi <42316228+k-tokitoh@users.noreply.github.com>
2022-07-10 23:03:57 +0900

    quit on ctrl+q

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,6 +1,11 @@
 use std::io::{self, stdout, Read};
 use termion::raw::IntoRawMode;
 
+fn ctrl_to_byte(c: char) -> u8 {
+    let byte = c as u8;
+    byte & 0b0001_1111
+}
+
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
@@ -12,8 +17,7 @@ fn main() {
         } else {
             println!("{:?} ({}) \r", b, c);
         }
-
-        if c == 'q' {
+        if b == ctrl_to_byte('q') {
             break;
         }
     }
