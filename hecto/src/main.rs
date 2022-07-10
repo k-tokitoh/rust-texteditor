21b5833fb0c112c2923c756724fa94236351057d
takashi <42316228+k-tokitoh@users.noreply.github.com>
2022-07-10 23:17:04 +0900

    handle errors

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,6 +1,10 @@
 use std::io::{self, stdout, Read};
 use termion::raw::IntoRawMode;
 
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
+
 fn ctrl_to_byte(c: char) -> u8 {
     let byte = c as u8;
     byte & 0b0001_1111
@@ -10,15 +14,19 @@ fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({}) \r", b, c);
-        }
-        if b == ctrl_to_byte('q') {
-            break;
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} \r", b);
+                } else {
+                    println!("{:?} ({}) \r", b, c);
+                }
+                if b == ctrl_to_byte('q') {
+                    break;
+                }
+            }
+            Err(e) => die(e),
         }
     }
 }
