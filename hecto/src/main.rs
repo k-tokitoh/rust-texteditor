08b08e9a518182465e407e4f0d47e5cd7b3456cb
takashi <42316228+k-tokitoh@users.noreply.github.com>
2022-07-10 23:40:53 +0900

    use Stdin#keys()

--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,31 +1,24 @@
-use std::io::{self, stdout, Read};
+use std::io::{self, stdout};
+use termion::event::Key;
+use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
 fn die(e: std::io::Error) {
     panic!("{}", e);
 }
 
-fn ctrl_to_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
-
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
-    for b in io::stdin().bytes() {
-        match b {
-            Ok(b) => {
-                let c = b as char;
-                if c.is_control() {
-                    println!("{:?} \r", b);
-                } else {
-                    println!("{:?} ({}) \r", b, c);
+    for key in io::stdin().keys() {
+        match key {
+            Ok(key) => match key {
+                Key::Ctrl('q') => break,
+                Key::Char(c) => {
+                    println!("{:?} ({}) \r", c as u8, c);
                 }
-                if b == ctrl_to_byte('q') {
-                    break;
-                }
-            }
+                _ => println!("{:?}\r", key),
+            },
             Err(e) => die(e),
         }
     }
