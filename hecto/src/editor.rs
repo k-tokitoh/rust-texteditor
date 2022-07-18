11edcaa21455e8e0cbb91e89acc0e9de2efc75fb
takashi <42316228+k-tokitoh@users.noreply.github.com>
2022-07-18 21:04:00 +0900

    extract methods

--- hecto/src/editor.rs
+++ hecto/src/editor.rs
@@ -11,17 +11,29 @@ impl Editor {
     pub fn run(&self) {
         let _stdout = stdout().into_raw_mode().unwrap();
 
-        for key in io::stdin().keys() {
-            match key {
-                Ok(key) => match key {
-                    Key::Ctrl('q') => break,
-                    Key::Char(c) => {
-                        println!("{:?} ({}) \r", c as u8, c);
-                    }
-                    _ => println!("{:?}\r", key),
-                },
-                Err(e) => die(&e),
+        loop {
+            if let Err(err) = self.process_keypress() {
+                die(&err);
             }
+        }
+    }
+
+    // 入力に応じた処理を行う
+    fn process_keypress(&self) -> Result<(), io::Error> {
+        let pressed_key = read_key()?;
+        match pressed_key {
+            Key::Ctrl('q') => panic!("Program end"),
+            // TODO
+            _ => Ok(()),
+        }
+    }
+}
+
+// 標準入力から読み取るだけ
+fn read_key() -> Result<Key, std::io::Error> {
+    loop {
+        if let Some(key) = io::stdin().lock().keys().next() {
+            return key;
         }
     }
 }
