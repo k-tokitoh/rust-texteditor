9f8c632b24951b6af5d972db3e25f5ea623a061a
takashi <42316228+k-tokitoh@users.noreply.github.com>
2022-07-18 18:43:01 +0900

    extract editor to another file

---
+++ hecto/src/editor.rs
@@ -0,0 +1,27 @@
+use std::io::{self, stdout};
+use termion::{event::Key, input::TermRead, raw::IntoRawMode};
+
+pub struct Editor {}
+
+impl Editor {
+    pub fn run(&self) {
+        let _stdout = stdout().into_raw_mode().unwrap();
+
+        for key in io::stdin().keys() {
+            match key {
+                Ok(key) => match key {
+                    Key::Ctrl('q') => break,
+                    Key::Char(c) => {
+                        println!("{:?} ({}) \r", c as u8, c);
+                    }
+                    _ => println!("{:?}\r", key),
+                },
+                Err(e) => die(e),
+            }
+        }
+    }
+}
+
+fn die(e: std::io::Error) {
+    panic!("{}", e);
+}
--- hecto/src/main.rs
+++ hecto/src/main.rs
@@ -1,23 +1,7 @@
-use std::io::{self, stdout};
-use termion::{event::Key, input::TermRead, raw::IntoRawMode};
-
-fn die(e: std::io::Error) {
-    panic!("{}", e);
-}
+mod editor;
+use editor::Editor;
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap();
-
-    for key in io::stdin().keys() {
-        match key {
-            Ok(key) => match key {
-                Key::Ctrl('q') => break,
-                Key::Char(c) => {
-                    println!("{:?} ({}) \r", c as u8, c);
-                }
-                _ => println!("{:?}\r", key),
-            },
-            Err(e) => die(e),
-        }
-    }
+    let editor = Editor {};
+    editor.run();
 }
